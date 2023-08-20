a6fcf45c4e81b7dd9e425a9928d1126a06cee48e
Edwin Pabon <edwin.pab2@gmail.com>
2023-08-20 16:13:19 -0500

    datos primitivos y datos Wraper con sus ejemplos y descripcion

--- src/main/java/B1/Main.java
+++ src/main/java/B1/Main.java
@@ -3,6 +3,22 @@ package B1;
 public class Main {
 
     public static void main(String[] args) {
+       //Datos primitivos
+        int mun1=9;// primitivo
+        double num2=5.5;
+        boolean esAlto=false;
+        char caracter='e';
+        // Wrappers
+        Short valor= 32767;
+        Integer num1 =10; // Wrapper int
+        Double num10=3.4;  // double
+        Boolean esFalso=true; //bolean
+        Character caracterW='M'; // Wraper Caracter
+
+        String text="Hola Mundo"; //no es un graper en considerado como un dato normal
+
+
         System.out.println("Hola mundo parque Ps");
+        System.out.println("esto es un primitivo "+ num1);
     }
 }
